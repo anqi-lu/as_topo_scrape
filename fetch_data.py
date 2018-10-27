bcefc2aa3727778e8de54d8f34220ec5b381a5d0
lizeqian <lizeqian@gmail.com>
2018-10-27 18:12:45 -0400

    complete auto request

---
+++ fetch_data.py
@@ -0,0 +1,23 @@
+from download import get_data
+from fetch import get_urls
+from post_get_request import send_request
+import datetime
+
+
+if __name__ == "__main__":
+    lg_summary = 'data/urls_from_bgpsum.txt'
+    lg_neighbor = 'data/urls_from_bgpneighbors.txt'
+
+    lg_database = 'data/urls_from_lg_database.txt'
+    urls = get_urls(lg_neighbor)
+    for i, url in enumerate(urls):
+        if get_data(url):
+            try:
+                text = send_request(url)
+                dest = 'data/output/'+str(i)+'_'+str(datetime.datetime.now())+'.txt'
+                with open(dest, 'a') as f:
+                    f.write(url)
+                    f.write(text)
+            except ValueError as err:
+                print(err.args)
+            
--- post_get_request.py
+++ post_get_request.py
@@ -1,4 +1,5 @@
 import requests
+import bs4
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import urllib
@@ -74,6 +75,93 @@ def is_scroll(form):
     if len(form.find_all('select')) == 0:
         return False
     return True
+
+def bs4_get_string(nxt_el):
+    if type(nxt_el) == bs4.element.NavigableString:
+        nxt_el_str = nxt_el.strip()
+    elif type(nxt_el) == bs4.element.Tag:
+        nxt_el_str = nxt_el.text.strip()
+    else:
+        raise ValueError("form tag not exist")
+    return nxt_el_str
+    
+
+def inputs_process(inputs, data):
+    for inp in inputs:
+        if 'name' in inp.attrs:
+            key = inp.attrs['name']
+            if 'type' not in inp.attrs:
+                if key not in data:
+                    data[key]=[]
+                    data[key].append(['',''])
+                continue
+
+            if inp.attrs['type'].lower() == 'text':
+                if key not in data:
+                    data[key]=[]
+                data[key].append(['',''])
+                continue
+            
+            if 'value' in inp.attrs:
+                value = inp.attrs['value']
+            elif 'type' in inp.attrs:
+                if inp.attrs['type'].lower() == 'submit':
+                    value = 'Submit'
+                elif inp.attrs['type'].lower() == 'text':
+                    value = ''
+                
+            text = ''
+            if inp.attrs['type'].lower() == 'radio':
+                nxt_el = inp.next_element
+                nxt_el_str = bs4_get_string(nxt_el)
+                    
+                while len(nxt_el_str) == 0:
+                    nxt_el = nxt_el.next_element
+                    if type(nxt_el) == bs4.element.Tag:
+                        if nxt_el.name.lower() == 'input':
+                            break
+                    nxt_el_str = bs4_get_string(nxt_el)
+                text = nxt_el_str
+            if key not in data:
+                data[key]=[]
+            data[key].append([value, text])
+
+def select_process(selects, data):
+    for select in selects:
+        key = select.attrs['name']
+        options = select.find_all('option')
+        for option in options:
+            value = option.attrs['value']   
+            text=''
+            nxt_el = option.next_element
+            nxt_el_str = bs4_get_string(nxt_el)
+            while len(nxt_el_str) == 0:
+                nxt_el = nxt_el.next_element
+                if type(nxt_el) == bs4.element.Tag:
+                    if nxt_el.name.lower() == 'option':
+                        break
+                nxt_el_str = bs4_get_string(nxt_el)
+            text = nxt_el_str
+            if key not in data:
+                data[key]=[]
+            data[key].append([value, text])
+            
+def data_postprocess(data):
+    post_data = {}
+    for key in data:
+        attrs = data[key]
+        if len(attrs) == 1:
+            post_data[key] = attrs[0][0]
+        else:
+            post_data[key] = attrs[0][0]
+            for attr in attrs:
+                text = attr[1]
+                if 'bgp' in text and '6' not in text and 'sum' in text: #BGP summary
+                    post_data[key] = attr[0]
+                    break
+    return post_data
+                
+                
 # =============================================================================
 # def read_url_data(filepath):
 #     data = {}
@@ -94,10 +182,22 @@ def is_scroll(form):
 def send_request(url):
     #get form contents inside the html
     r = requests.get(url)
-    r_text = r.text
-    soup = BeautifulSoup(r_text, 'html.parser')
+    html = r.content.decode('utf-8', 'ignore')
+#    r_text = r.text
+    soup = BeautifulSoup(html, 'html.parser')
     #all the needed info is inside the [form] frame
-    form = soup.find_all('form')[0]
+    forms = soup.find_all('form')
+    if len(forms) == 0:
+        raise ValueError("form tag not exist")
+    form = forms[0]
+
+    selects = form.find_all('select')
+    inputs = form.find_all('input')
+    
+    
+#    if not(len(selects) == 1 or len(tables) == 1):
+#        raise ValueError("Form structure violation", len(selects), len(tables))
+    
     #determine method (GET or POST)
     if 'method' in form.attrs:
         method = form.attrs['method']
@@ -108,44 +208,37 @@ def send_request(url):
         endpoint = form.attrs['action']
     else:
         endpoint = ''
+        
     #create data lib to store request info
     data = {}
+    
+    inputs_process(inputs, data)
+    select_process(selects, data)
+    
+    post_data = data_postprocess(data)
 
     #if it's a post method
     if method.upper() == 'POST':
         post_url = urljoin(url, endpoint)   #join sources url with endpoint to create query path
-        if is_scroll(form):     #scroll to select bgp summary           
-            #find <input> -> <name>, store (name: value) to data
-            input_name_todata(form, data)
-            #find <select> -> <option> -> show bgp summary    
-            select_option_todata(form, data)
-        else:   #inside table to point bgp summary
-            #find table
-            table_input_todata(form, data)
-        #call send post request function send_pst_req()
-        #import pdb; pdb.set_trace() 
-        return_text = send_pst_req([post_url, data])
+        return_text = send_pst_req([post_url, post_data])
 #   if it's a get method
     elif method.upper() == 'GET':
         get_url = urljoin(url, endpoint)+'?'
-        #find <select> -> <option> -> show bgp summary
-        if is_scroll(form):
-            select_option_todata(form, data)        
-            #find <input> -> <name>, store (name: value) to data
-            input_name_todata(form, data)
-        else:
-            table_input_todata(form, data)
         #add all info in data to the source url to form a new request url   
-        for item in data:
-            get_url = get_url + item +'='+ urllib.parse.quote_plus(data[item]) +'&'
+        for item in post_data:
+            get_url = get_url + item +'='+ urllib.parse.quote_plus(post_data[item]) +'&'
         #call send get request function send_get_req()
-  
         return_text = send_get_req(get_url)
-    
-    return return_text
+        
+    response = BeautifulSoup(return_text, 'html.parser')
+    pres = response.find_all('pre')
+    if len(pres) > 0:
+        return pres[0].text
+    else:
+        return return_text
     
 
     
 if __name__ == "__main__":
-    url = 'http://lg.alsysdata.net'
+    url = 'https://lg.maxiweb.com.br/cgi-local/lg.cgi'
     resp = send_request(url)
